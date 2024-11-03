96672a22cca8f3641a579cc32dab750236400f8b
Jesus Montoya <jesmonsa2018@gmail.com>
2024-11-02 23:25:17 -0500

    Ultimo commit

--- leccion1_servidor_web_unico/provider.tf
+++ leccion1_servidor_web_unico/provider.tf
@@ -8,20 +8,10 @@ terraform { # Versión de Terraform.
 }
 # General Provider 
 provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid 
-  user_ocid        = var.user_ocid 
-  fingerprint      = var.fingerprint 
-  private_key_path = var.private_key_path 
-  region           = var.region 
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
 
-# Home Region Provider
-provider "oci" {
-  alias                = "homeregion" 
-  tenancy_ocid         = var.tenancy_ocid 
-  user_ocid            = var.user_ocid 
-  fingerprint          = var.fingerprint 
-  private_key_path     = var.private_key_path 
-  region               = var.region
-  disable_auto_retries = "true" 
-}
