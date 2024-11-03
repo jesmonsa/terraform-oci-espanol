aa23ede6c907a85b772959dfc243dfa4a71f0548
Jesus Montoya <jesmonsa2018@gmail.com>
2024-11-02 22:33:17 -0500

    Ultimo commit

--- leccion1_servidor_web_unico/provider.tf
+++ leccion1_servidor_web_unico/provider.tf
@@ -1,4 +1,12 @@
 # Configuración del proveedor OCI 
+terraform { # Versión de Terraform.
+  required_providers { # Proveedores requeridos.
+    oci = {
+      source = "oracle/oci" # Fuente del proveedor de OCI.
+      version = "5.35.0" # Versión del proveedor de OCI.
+    }
+  }
+}
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
